ad539e700c275394bbeaf5be9c1513e73dc483a4
tmneth <80415416+tmneth@users.noreply.github.com>
2022-06-13 14:52:46 +0300

    Update shell script for the setup

--- nvim-config.sh
+++ nvim-config.sh
@@ -1,11 +1,17 @@
 # !/bin/bash
 
+# sudo paru -S neovim nerd-fonts-complete ccls
+
 curl -fLo   $HOME/.local/share/nvim/site/autoload/plug.vim --create-dirs \
 https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim 
 
-mkdir ~/.config/nvim/init.vim
-mkdir ~/.config/nvim/after
-kdir ~/.config/nvim/colors
+git clone https://github.com/overcache/NeoSolarized.git
+cd NeoSolarized/colors && mv NeoSolarized.vim ~/.config/nvim/colors/
 
-curl --output ~/.config/nvim/init.vim https://raw.githubusercontent.com/tmneth/neovim-config/main/config/init.vim
-curl --output ~/.config/nvim/plug.vim https://raw.githubusercontent.com/tmneth/neovim-config/main/config/plug.vim
+mkdir ~/.config/nvim/{plugins,colors}
+
+git clone https://github.com/tmneth/neovim-config
+mv ~/neovim-config/config/{init.vim,plug.vim,maps.vim} ~/.config/nvim
+
+cd $HOME/.local/share/nvim/plugged/coc.nvim && yarn install && yarn build
+# :CocInstall coc-tsserver coc-eslint coc-tslint coc-tslint-plugin
