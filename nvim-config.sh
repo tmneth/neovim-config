7f238c5d246cf082bd05f1fa6b1b234bf99661c4
tmneth <80415416+tmneth@users.noreply.github.com>
2022-06-13 15:23:19 +0300

    Add PlugInstall non-interactively from cli

--- nvim-config.sh
+++ nvim-config.sh
@@ -1,17 +1,20 @@
 # !/bin/bash
 
-# sudo paru -S neovim nerd-fonts-complete ccls
+# sudo paru -S neovim nerd-fonts-complete lua ccls 
 
 curl -fLo   $HOME/.local/share/nvim/site/autoload/plug.vim --create-dirs \
 https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim 
 
 git clone https://github.com/overcache/NeoSolarized.git
-cd NeoSolarized/colors && mv NeoSolarized.vim ~/.config/nvim/colors/
+mv NeoSolarized/colors/NeoSolarized.vim ~/.config/nvim/colors/
 
 mkdir ~/.config/nvim/{plugins,colors}
 
 git clone https://github.com/tmneth/neovim-config
-mv ~/neovim-config/config/{init.vim,plug.vim,maps.vim} ~/.config/nvim
+mv neovim-config/config/{init.vim,plug.vim,maps.vim} ~/.config/nvim
 
-cd $HOME/.local/share/nvim/plugged/coc.nvim && yarn install && yarn build
-# :CocInstall coc-tsserver coc-eslint coc-tslint coc-tslint-plugin
+nvim +'PlugInstall --sync' +qa
+nvim +'CocInstall --sync coc-tsserver coc-eslint coc-tslint coc-tslint-plugin coc-json' +qa
+nvim +CocUpdateSync +qa
+
+# cd $HOME/.local/share/nvim/plugged/coc.nvim && yarn install && yarn build
