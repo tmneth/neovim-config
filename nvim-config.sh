c180344dd45cde9b062603bd0b25bd4dfeb9b299
tmneth <80415416+tmneth@users.noreply.github.com>
2022-06-15 16:12:10 +0300

    Replace mv with curl

--- nvim-config.sh
+++ nvim-config.sh
@@ -1,6 +1,6 @@
 # !/bin/bash
 
-# sudo paru -S neovim nerd-fonts-complete lua ccls yarn
+# sudo paru -S neovim nerd-fonts-complete lua ccls npm
 
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
 https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim 
@@ -8,12 +8,14 @@ https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 curl -fLo ~/.config/nvim/colors/NeoSolarized.vim --create-dirs \
 https://raw.githubusercontent.com/overcache/NeoSolarized/master/colors/NeoSolarized.vim
 
-git clone https://github.com/tmneth/neovim-config
-mv neovim-config/config/{init.vim,plug.vim,maps.vim} ~/.config/nvim
+curl https://raw.githubusercontent.com/tmneth/neovim-config/main/config/init.vim  > ~/.config/nvim/init.vim
+curl https://raw.githubusercontent.com/tmneth/neovim-config/main/config/plug.vim  > ~/.config/nvim/plug.vim
+curl https://raw.githubusercontent.com/tmneth/neovim-config/main/config/maps.vim  > ~/.config/nvim/maps.vim
+
 
 nvim +'PlugInstall --sync' +qa
 nvim +'CocInstall --sync coc-html coc-css coc-tsserver coc-eslint coc-tslint coc-tslint-plugin coc-json' +qa
 nvim +CocUpdateSync +qa
 
-cd ~/.local/share/nvim/plugged/coc.nvim && yarn install
-cd ~/.local/share/nvim/plugged/vim-prettier && yarn install
+npm install --prefix ~/.local/share/nvim/plugged/coc.nvim
+npm install --prefix ~/.local/share/nvim/plugged/vim-prettier
